d1759578576c0cb1cfd6a26daf613f5f9b528fc7
Alex Biggers <alex.j.biggers@gmail.com>
2019-06-20 10:40:08 -0700

    initial commit

---
+++ precache-manifest.ba049b148cfcf3cf9eb44bb325d51436.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/react.5d5d9eef.svg"
+  },
+  {
+    "revision": "c931141e8d281bc7328c",
+    "url": "/static/css/main.1786256c.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae0925dac0e8e5ac962b",
+    "url": "/static/js/2.4491b10c.chunk.js"
+  },
+  {
+    "revision": "7e252e6299a92fdf2e2cbfca700f2f4a",
+    "url": "/static/media/python.7e252e62.svg"
+  },
+  {
+    "revision": "95c2b9587e610e97f667099f2c859ecf",
+    "url": "/static/media/nodejs.95c2b958.svg"
+  },
+  {
+    "revision": "8dc2aabd124b7b6c6b68c7934fec61c1",
+    "url": "/static/media/d3.8dc2aabd.svg"
+  },
+  {
+    "revision": "c931141e8d281bc7328c",
+    "url": "/static/js/main.4a7c03f8.chunk.js"
+  },
+  {
+    "revision": "f80a91ba03fbf0e99ea6949aa3a6399e",
+    "url": "/static/media/mysql.f80a91ba.svg"
+  },
+  {
+    "revision": "2985235df31b8742d893d5b7e80145c5",
+    "url": "/static/media/mongodb.2985235d.svg"
+  },
+  {
+    "revision": "ae835dd9d736f8393d660438edbea47c",
+    "url": "/static/media/numpy.ae835dd9.svg"
+  },
+  {
+    "revision": "f142ba0db8814cf4d8849f8f8e41e9be",
+    "url": "/static/media/matplotlib.f142ba0d.svg"
+  },
+  {
+    "revision": "3a090741999c0467a877b078c6c2ca58",
+    "url": "/static/media/ubuntu.3a090741.svg"
+  },
+  {
+    "revision": "9b45d8a8ab2bac8cb4109bd75592f2f7",
+    "url": "/static/media/Screenshot.9b45d8a8.png"
+  },
+  {
+    "revision": "3b65cf0bbe53a7ef149227a66a136c11",
+    "url": "/index.html"
+  }
+];
